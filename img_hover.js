0f72769406700d7a97f531a939e1d316bd6c1133
adarsh-k-d <adarshkdil2003@gmail.com>
2024-09-26 20:32:49 +0530

    Third Commit

---
+++ img_hover.js
@@ -0,0 +1,8 @@
+function foc(){
+    var imgtex = document.getElementById("azuretex")
+    imgtex.innerHTML="Name"
+}
+function unfoc(){
+    var imgtex = document.getElementById("azuretex")
+    imgtex.innerHTML=" "
+}
