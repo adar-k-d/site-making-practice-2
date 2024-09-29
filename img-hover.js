f21e4fdaf1bf463be1f9b13672b78e7f7508b1de
adarsh-k-d <adarshkdil2003@gmail.com>
2024-09-29 18:00:33 +0530

    Sixth  Commit

--- img-hover.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByClassName("fixed-size-image").addEventListener("mouseover",function(event) {
-    let cid=getElementsByClassName("fixed-size-image");
-    cid.innerHTML('<h4 class="view" style="position:relative;display:inline-block;">VIEW</h4>')
-
-})
-document.getElementsByClassName("fixed-size-image").addEventListener("mouseout",function(event) {
-    let cid=getElementsByClassName("fixed-size-image");
-    cid.innerHTML('')
-})
