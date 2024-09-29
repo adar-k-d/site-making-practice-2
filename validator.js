729984d55a05bafa56cd3aa76a955dceceb330ee
adarsh-k-d <adarshkdil2003@gmail.com>
2024-09-29 18:25:23 +0530

    Seventh  Commit

--- validator.js
+++ validator.js
@@ -14,3 +14,21 @@ document.getElementById("book-form").addEventListener("submit",function(event) {
     }
 
 });
+
+document.getElementById("book-form").addEventListener("submit",function(event) {
+    const nme=document.getElementById("name").value.trim();
+    const email=document.getElementById("email").value.trim();
+    const phone=document.getElementById("phone").value.trim();
+    const pclink=document.getElementById("pclink").value;
+    const partlist=document.getElementById("pclist").value;
+    const addr=document.getElementById("addr").value.trim();
+    if(!nme || !email || !phone || (!pclink && !partlist) || !addr){
+        event.preventDefault();
+        alert("Please fill out all details before placing an order!")
+    }
+    else{
+        console.log("Success")
+        window.location.href="success.html";
+    }
+
+});
